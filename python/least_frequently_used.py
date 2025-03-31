10264d7f2cb36f9c98c74e3c2c50f80b6913a767
duytungqua <quangduy.hcmut@gmail.com>
2025-03-31 23:53:50 +0700

    algo: add new class and two method

---
+++ python/least_frequently_used.py
@@ -0,0 +1,12 @@
+"""
+Initialize with a cache n size
+set(key, value): set key to value 
+get(key): get the value of key
+"""
+
+class LFUCache:
+
+    def set(self, key, value):
+       
+    def get(self, key):
+        
