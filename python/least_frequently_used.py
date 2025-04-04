642059dd2b8ea26ed77265eb2051527deaa47417
duytungqua <quangduy.hcmut@gmail.com>
2025-04-04 17:24:32 +0700

    (leetcode): init gratest common divisor string

--- python/least_frequently_used.py
+++ python/least_frequently_used.py
@@ -5,25 +5,4 @@ get(key): get the value of key
 """
 
 class LFUCache:
-    def __init__(self, n):
-        self.cache = {}
-        self.freq = {}
-        self.n = n
-
-    def set(self, key, value):
-        if len(self.cache) == self.n:
-            min_freq = min(self.freq.values())
-            for k, v in self.freq.items():
-                if v == min_freq:
-                    del self.cache[k]
-                    del self.freq[k]
-                    break
-        self.cache[key] = value
-        self.freq[key] = 0
-
-    def get(self, key):
-        if key in self.cache:
-            self.freq[key] += 1
-            return self.cache[key]
-        
-        return -1
+    
