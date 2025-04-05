b4c879a2f92cad63e77d3e4b474bc8c64bc13d89
duytungqua <quangduy.hcmut@gmail.com>
2025-04-05 21:39:05 +0700

    (coding): add new coding today

--- python/least_frequently_used.py
+++ python/least_frequently_used.py
@@ -5,4 +5,21 @@ get(key): get the value of key
 """
 
 class LFUCache:
-    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.freq = {}
+        self.min_freq = 0
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        value, freq = self.cache[key]
+        self.freq[freq].remove(key)
+        if not self.freq[freq]:
+            del self.freq[freq]
+            if freq == self.min_freq:
+                self.min_freq += 1
+        self.freq.setdefault(freq + 1, []).append(key)
+        self.cache[key] = (value, freq + 1)
+        return value
