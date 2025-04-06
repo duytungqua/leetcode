aef9055621909bce0c711941661b9e47827a9fc6
duytungqua <quangduy.hcmut@gmail.com>
2025-04-06 22:25:22 +0700

    leetcode: pseudo code euclidean

---
+++ python/euclidean.py
@@ -0,0 +1,11 @@
+
+class Euclidean: 
+    #pseudo code:
+    """
+    func gcd(a,b):
+        while b != 0:
+            tem =  b
+            b = a% b
+            a = tem
+        return a
+    """
