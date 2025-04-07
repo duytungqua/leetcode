2bea0ef71899b846c4704ea1c0412e25d4813e91
duytungqua <quangduy.hcmut@gmail.com>
2025-04-07 22:48:31 +0700

    leetcode: impl euclidean code

--- python/euclidean.py
+++ python/euclidean.py
@@ -9,3 +9,12 @@ class Euclidean:
             a = tem
         return a
     """
+    def gcd():
+        a = 10
+        b = 5
+        while b != 0:
+            temp = b
+            b = a % 10
+            a = temp
+        return a
+    #test
