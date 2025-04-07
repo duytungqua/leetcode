c000aa7e0c5fbd678c77992c8c8580249be43680
duytungqua <quangduy.hcmut@gmail.com>
2025-04-07 23:02:06 +0700

    leetcode: add code impl

--- python/euclidean.py
+++ python/euclidean.py
@@ -23,3 +23,17 @@ class Euclidean:
             return ""
         len = gcd(len(str1), len(str2)) 
         return str1[:len]
+
+    """
+    def gcdOfStrings(self, str1, str2):
+        if str1 + str2 != str2 + str1:
+            return ""
+        len_str1 = len(str1)
+        len_str2 = len(str2)
+
+        while len_str2 != 0:
+            temp = len_str2
+            len_str2 = len_str1 % len_str2
+            len_str1 = temp
+        return str1[:len_str1]
+    """
