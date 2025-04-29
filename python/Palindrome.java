0fcf7ef242babfdcebc173e8b3dccc67e07b5c2a
duytungqua <quangduy.hcmut@gmail.com>
2025-04-29 23:44:39 +0700

    leetcode: refactor palindrome string

--- python/Palindrome.java
+++ python/Palindrome.java
@@ -2,27 +2,18 @@ package python;
 
 public class Palindrome {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                sb.append(Character.toLowerCase(c));
-            }
+        s = s.toLowerCase();
+        
+        //reverse the string
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = s.length() - 1; i>=0; i--) {
+            sb.append(String.valueOf(s.charAt(i)));
         }
-        String str = sb.toString();
-        int left = 0;
-        int right = str.length() - 1;
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        return sb.toString().equals(s);
     }
 
     public static void main(String[] args) {
         Palindrome p = new Palindrome();
-        String s
+        String s = "hello";
+        p.isPalindrome(s);
 }
